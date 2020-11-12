2588593c4f975636bab6a260f50c06e8c4381baa
luis saldana <tluis10a@gmail.com>
2020-11-11 16:32:38 -0800

    structured english

---
+++ lab4.py
@@ -0,0 +1,10 @@
+"""
+This program allows a user three tries to guess the correct answer to the question  
+Question = "what is the capital of california". the answer is "sacramento".
+
+we first set max_tries = 3. then we create a loop to iterate three times. for each iteration, we ask the user for the answer (user input).  then base on the answer the user gives, we check to see if the user input matches the asnwer. If so, print "correct", the terminate the loop with a break statement
+
+if the user could not guess the correct answer within the max_tries, then print "you have used up your allotment of guesses.", then print" the correct answer is "california"
+
+""" 
+print("testing")
